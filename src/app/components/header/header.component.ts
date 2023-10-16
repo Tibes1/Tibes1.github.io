d71de5ae548d0369f4ba0deeacae4e1836ccfe3d
Tibes <tibes93@hotmail.com>
2023-10-16 18:07:39 -0300

    pre smoth scroll

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -7,4 +7,20 @@ import { Component } from '@angular/core';
 })
 export class HeaderComponent {
 
+  hamMenuBtn = document.querySelector('.header__main-ham-menu-cont') as HTMLElement;
+  smallMenu = document.querySelector('.header__sm-menu') as HTMLElement;
+  headerHamMenuBtn = document.querySelector('.header__main-ham-menu') as HTMLElement;
+  headerHamMenuCloseBtn = document.querySelector('.header__main-ham-menu-close') as HTMLElement;
+  headerSmallMenuLinks = document.querySelectorAll('.header__sm-menu-link');
+  
+  displayNone:string = 'd-none'
+
+  showDiv: boolean = false;
+
+  toggleDiv() {
+    this.showDiv = !this.showDiv;
+  }
+
+  
+
 }
